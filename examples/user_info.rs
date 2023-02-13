6b0f6ed5340b4ea6a09628cba79ba97aa847440a
GitBluub <louis.auzuret@gmail.com>
2023-02-13 14:47:08 +0900

    feat: use WanikaniClient in examples

--- examples/user_info.rs
+++ examples/user_info.rs
@@ -1,11 +1,9 @@
-use wanikani_rs::response::UserResponse;
+use wanikani_rs::{response::UserResponse, wanikani_client::WanikaniClient};
 
 #[tokio::main]
 async fn main() -> Result<(), reqwest::Error> {
-    let client = reqwest::Client::new();
-    let resp: UserResponse = client
-        .get("https://api.wanikani.com/v2/user")
-        .bearer_auth("a6453799-9601-483b-8976-492e87b1c46a").send().await?.json().await?;
+    let client = WanikaniClient::default();
+    let resp: UserResponse = client.get_user_info().await?;
     println!("{:#?}", resp);
     Ok(())
 }
