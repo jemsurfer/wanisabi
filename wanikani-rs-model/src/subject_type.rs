aadaeb43f202e546e1b7b3516aa28b19ab9b6a22
GitBluub <louis.auzuret@gmail.com>
2023-02-15 20:23:35 +0900

    feat: partial eq for assignment and subject_type

--- wanikani-rs-model/src/subject_type.rs
+++ wanikani-rs-model/src/subject_type.rs
@@ -1,9 +1,9 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 #[serde(rename_all = "snake_case")]
 pub enum SubjectType {
     Kanji,
     Radical,
-    Vocabulary
+    Vocabulary,
 }
