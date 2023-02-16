f395ff820646b5b8d825485336dd952ac1808604
GitBluub <louis.auzuret@gmail.com>
2023-02-16 15:51:00 +0900

    feat: rework of get_user using macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,8 @@
+#![feature(concat_idents)]
 pub use wanikani_rs_model as model;
 
 pub mod response;
 pub mod wanikani_client;
+
+#[macro_use]
+pub mod wrapper;
--- src/wanikani_client.rs
+++ src/wanikani_client.rs
@@ -1,7 +1,6 @@
-use std::env;
-
 use reqwest::Result;
-use serde::de;
+use serde::{de, Serialize};
+use std::env;
 
 use crate::model::assignment::Assignment;
 use crate::response::{CollectionResponse, ResourceResponse, UserResponse};
@@ -10,46 +9,73 @@ use std::fmt::Debug;
 #[derive(Clone, Debug)]
 pub struct WanikaniClient {
     pub key: String,
-    client: reqwest::Client,
+    pub client: reqwest::Client,
 }
 
-macro_rules! GET {
-    ($self:ident, $route:expr) => {
-        $self._get($route).await
-    };
-}
+pub mod macros {
+    #[macro_export]
+    macro_rules! get {
+        ($name:tt, $route:expr, $query:ty, $return:ty) => {
+            pub async fn $name(&self, query: &Vec<($query, &str)>) -> Result<$return, Error> {
+                let req = self
+                    .client
+                    .get(format!("https://api.wanikani.com/v2/{}", $route))
+                    .bearer_auth(self.key.to_owned())
+                    .query(query);
+                req.send().await?.json().await
+            }
+        };
+        ($name:tt, $route:expr, $return:ty) => {
+            pub async fn $name(&self) -> Result<$return, Error> {
+                let req = self
+                    .client
+                    .get(format!("https://api.wanikani.com/v2/{}", $route))
+                    .bearer_auth(self.key.to_owned());
+                req.send().await?.json().await
+            }
+        };
+    }
 
+    #[macro_export]
+    macro_rules! post {
+        ($name:tt, $route:expr, $body:ty, $return:ty) => {
+            pub async fn $name(&self, body: $body) -> Result<$return, Error> {
+                let req = self
+                    .client
+                    .post("https://api.wanikani.com/v2/".to_owned() + $route)
+                    .bearer_auth(self.key.to_owned());
+                req.send().await?.json().await
+            }
+        };
+    }
+}
 impl WanikaniClient {
     pub fn new(key: String) -> Self {
         let client = reqwest::Client::new();
         Self { key, client }
     }
-    async fn _get<T>(&self, url: &str) -> Result<T>
-    where
-        T: de::DeserializeOwned + Debug,
-    {
-        self.client
-            .get("https://api.wanikani.com/v2".to_string() + url)
-            .bearer_auth(self.key.to_owned())
-            .send()
-            .await?
-            .json()
-            .await
-    }
+    // async fn _get<T, Q>(&self, url: &str, query: &Q) -> Result<T>
+    // where
+    //     T: de::DeserializeOwned + Debug,
+    //     Q: Serialize + ?Sized,
+    // {
+    //     let req = self
+    //         .client
+    //         .get("https://api.wanikani.com/v2".to_string() + url)
+    //         .bearer_auth(self.key.to_owned())
+    //         .query(query);
+    //     req.send().await?.json().await
+    // }
 
-    pub async fn get_user_info(&self) -> Result<UserResponse> {
-        GET!(self, "/user")
-    }
+    // pub async fn get_assignments(
+    //     &self,
+    // ) -> Result<CollectionResponse<ResourceResponse<Assignment>>> {
+    //     GET!(self, "/assignments")
+    // }
 
-    pub async fn get_assignments(
-        &self,
-    ) -> Result<CollectionResponse<ResourceResponse<Assignment>>> {
-        GET!(self, "/assignments")
-    }
-
-    pub async fn get_assignment(&self, id: i32) -> Result<ResourceResponse<Assignment>> {
-        GET!(self, format!("/assignments/{id}").as_str())
-    }
+    // pub async fn get_assignment(&self, id: i32) -> Result<ResourceResponse<Assignment>> {
+    //     GET!(self, format!("/assignments/{id}").as_str())
+    // }
 }
 
 impl Default for WanikaniClient {
