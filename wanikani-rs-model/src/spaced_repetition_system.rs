6e536652bb9db607f888d096dc2391f710b0bb4f
GitBluub <louis.auzuret@gmail.com>
2023-02-18 15:16:58 +0900

    feat: SRS model

---
+++ wanikani-rs-model/src/spaced_repetition_system.rs
@@ -0,0 +1,21 @@
+use chrono::{prelude::DateTime, Utc};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct SrsStage {
+    pub interval: Option<i64>,
+    pub interval_unit: Option<String>,
+    pub position: i64,
+}
+
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct SpacedRepetitionSystem {
+    pub burning_stage_position: i64,
+    pub created_at: DateTime<Utc>,
+    pub description: String,
+    pub name: String,
+    pub passing_stage_position: i64,
+    pub stages: Vec<SrsStage>,
+    pub starting_stage_position: i64,
+    pub unlocking_stage_position: i64,
+}
