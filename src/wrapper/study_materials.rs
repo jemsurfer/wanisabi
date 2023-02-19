78b886dfd6b8a1aec415b62530dbccefd1beca6a
GitBluub <louis.auzuret@gmail.com>
2023-02-19 22:35:23 +0900

    feat: study material create and update

--- src/wrapper/study_materials.rs
+++ src/wrapper/study_materials.rs
@@ -1,5 +1,5 @@
 use crate::{
-    get,
+    get, post, put,
     response::{CollectionResponse, ResourceResponse},
     wanikani_client::WanikaniClient,
     wanikani_client::QP,
@@ -19,6 +19,21 @@ pub enum StudyMaterialFilter {
     UpdatedAfter(DateTime<Utc>),
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct StudyMaterialCreate {
+    pub subjet_id: i64,
+    pub meaning_note: Option<String>,
+    pub reading_note: Option<String>,
+    pub meaning_synonyms: Option<Vec<String>>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct StudyMaterialUpdate {
+    pub meaning_note: Option<String>,
+    pub reading_note: Option<String>,
+    pub meaning_synonyms: Option<Vec<String>>,
+}
+
 impl WanikaniClient {
     get!(
         get_study_materials_filtered,
@@ -34,6 +49,19 @@ impl WanikaniClient {
     get!(
         get_study_material,
         "study_materials/{id}",
+        ResourceResponse<StudyMaterial>,
+        id: i64
+    );
+    post!(
+        create_study_material,
+        "study_materials/",
+        StudyMaterialCreate,
+        ResourceResponse<StudyMaterial>
+    );
+    put!(
+        update_study_material,
+        "study_materials/{id}",
+        StudyMaterialUpdate,
         ResourceResponse<StudyMaterial>,
         id: i64
     );
