7bf617e99ccbaf56aaeea5960f71579d92a4393a
Phusit Somboonyingsuk <phusitsom@gmail.com>
2023-07-12 13:41:28 +0700

    Add `kana_vocabulary` to subject type

--- wanikani-rs-model/src/subject_type.rs
+++ wanikani-rs-model/src/subject_type.rs
@@ -3,6 +3,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
 #[serde(rename_all = "snake_case")]
 pub enum SubjectType {
+    KanaVocabulary,
     Kanji,
     Radical,
     Vocabulary,
