bb00b7f0936b42d6d25b6a357afc993f62ec9321
Jeremy Petch <jeremy.petch@outlook.com>
2024-02-17 11:00:39 +0000

    Add params for new() method

--- src/client.rs
+++ src/client.rs
@@ -10,8 +10,8 @@ use std::{env, time::Duration};
 pub struct Client {
     pub key: String,
     pub client: reqwest::Client,
-    pub rate_limiter: Ratelimiter,
-    pub db_pool: SqlitePool,
+    pub rate_limiter: Option<Ratelimiter>,
+    pub pool: Option<SqlitePool>,
 }
 
 #[serde_with::serde_as]
@@ -25,11 +25,21 @@ pub mod macros {
     //Otherwise parse it to an error
     //Otherwise return a parsing/network error
     #[macro_export]
-    macro_rules! process_response {
-        ($return:ty, $res:ident, $self:ident) => {{
-            if let Err(sleep) = $self.rate_limiter.try_wait() {
-                tokio::time::sleep(sleep).await;
+    macro_rules! check_query {
+        ($self:ident, $route:expr) => {
+            if let Some(limiter) = &$self.rate_limiter {
+                if let Err(sleep) = limiter.try_wait() {
+                    tokio::time::sleep(sleep).await;
+                }
             }
+            if let Some(pool) = &$self.pool {
+                //Check whether we've already got the result and it's up to date
+            }
+        };
+    }
+    #[macro_export]
+    macro_rules! process_response {
+        ($return:ty, $res:ident) => {{
             if let Ok(res) = serde_json::from_str::<$return>(&$res) {
                 Ok(res)
             } else {
@@ -39,6 +49,7 @@ pub mod macros {
     }
     #[macro_export]
     macro_rules! get {
+        //Single or unfiltered queries
         ($name:tt, $route:expr, $return:ty $(, $v:tt: $t:ty)*) => {
             pub async fn $name(&self $(, $v: $t)*) -> Result<$return, $crate::Error> {
                 let url = String::from("https://api.wanikani.com/v2/") + &(format!($route));
@@ -47,9 +58,10 @@ pub mod macros {
                     .get(url)
                     .bearer_auth(self.key.to_owned());
                 let res = req.send().await?.text().await?;
-                process_response!($return, res, self)
+                process_response!($return, res)
             }
         };
+        //Filtered queries
         ($name:tt, $route:expr, $query:ty, $return:ty $(, $v:tt: $t:ty)*) => {
             pub async fn $name(&self, query: Vec<$query> $(, $v: $t)*) -> Result<$return, $crate::Error> {
                 let qp = QueryProcessor(query);
@@ -80,12 +92,17 @@ pub mod macros {
                     .build()?;
                 req.url_mut().set_query(Some(&qs));
                 let res = self.client.execute(req).await?.text().await?;
-                process_response!($return, res, self)
+                process_response!($return, res)
             }
         };
     }
     #[macro_export]
     macro_rules! post {
+        //Post requests need to be wrapped in a field matching the endpoint name.
+        //E.g. PUT assignments needs to be in the form:
+        //{
+        //  assignments: {..}
+        //}
         ($name:tt, $route:expr, $body:ty, $return:ty, $wrapper: ident, $attr: ident $(,$v:tt: $t:ty)*) => {
             pub async fn $name(&self, body: $body $(, $v: $t)*) -> Result<$return, $crate::Error> {
                 let wrapped = $wrapper{
@@ -97,23 +114,13 @@ pub mod macros {
                     .bearer_auth(self.key.to_owned())
                     .json(&wrapped);
                 let res = req.send().await?.text().await?;
-                process_response!($return, res, self)
-            }
-        };
-        ($name:tt, $route:expr, $body:ty, $return:ty $(,$v:tt: $t:ty)*) => {
-            pub async fn $name(&self, body: &$body $(, $v: $t)*) -> Result<$return, $crate::Error> {
-                let req = self
-                    .client
-                    .post("https://api.wanikani.com/v2/".to_owned() + &(format!($route)))
-                    .bearer_auth(self.key.to_owned())
-                    .json(body);
-                let res = self.client.execute(req).await?.text().await?;
-                process_response!($return, res, self)
+                process_response!($return, res)
             }
         };
     }
     #[macro_export]
     macro_rules! put {
+        //Most put requests need to be wrapped
         ($name:tt, $route:expr, $body:ty, $return:ty, $wrapper:ident, $attr: ident $(,$v:tt: $t:ty)*) => {
             pub async fn $name(&self, body: $body $(, $v: $t)*) -> Result<$return, $crate::Error> {
                 let wrapped = $wrapper{
@@ -125,9 +132,10 @@ pub mod macros {
                     .bearer_auth(self.key.to_owned())
                     .json(&wrapped);
                 let res = req.send().await?.text().await?;
-                process_response!($return, res, self)
+                process_response!($return, res)
             }
         };
+        //Apart from assignments
         ($name:tt, $route:expr, $body:ty, $return:ty $(,$v:tt: $t:ty)*) => {
             pub async fn $name(&self, body: &$body $(, $v: $t)*) -> Result<$return, $crate::Error> {
                 let req = self
@@ -136,43 +144,46 @@ pub mod macros {
                     .bearer_auth(self.key.to_owned())
                     .json(body);
                 let res = req.send().await?.text().await?;
-                process_response!($return, res, self)
+                process_response!($return, res)
             }
         };
     }
 }
 
 impl Client {
-    //The default trait cannot be derived for async functions
+    //The default trait cannot be derived for async functions, so in the main impl block
+    //By default, enable rate limiting & caching
     pub async fn default() -> Result<Self, crate::Error> {
         let client = reqwest::Client::new();
-        let (rate_limiter, db_pool) = rate_limiter_and_pool().await?;
+        let (rate_limiter, pool) = rate_limiter_and_pool(true, true).await?;
         match env::var_os("WANIKANI_API_KEY") {
             Some(key) => Ok(Self {
                 key: key.into_string().unwrap(),
                 client,
                 rate_limiter,
-                db_pool,
+                pool,
             }),
             None => Err(crate::Error::NoApiKey),
         }
     }
 
-    pub async fn new(key: String) -> Result<Self, crate::Error> {
+    pub async fn new(key: String, rate_limit: bool, cache: bool) -> Result<Self, crate::Error> {
         let client = reqwest::Client::new();
-        let (rate_limiter, db_pool) = rate_limiter_and_pool().await?;
+        let (rate_limiter, pool) = rate_limiter_and_pool(rate_limit, cache).await?;
         Ok(Self {
             key,
             client,
             rate_limiter,
-            db_pool,
+            pool,
         })
     }
 
     //Generic get function for other URLs
     pub async fn get(&self, url: String) -> Result<Response, reqwest::Error> {
-        if let Err(sleep) = self.rate_limiter.try_wait() {
-            tokio::time::sleep(sleep).await;
+        if let Some(limiter) = &self.rate_limiter {
+            if let Err(sleep) = limiter.try_wait() {
+                tokio::time::sleep(sleep).await;
+            }
         }
         Ok(self
             .client
@@ -183,16 +194,30 @@ impl Client {
     }
 }
 
-async fn rate_limiter_and_pool() -> Result<(Ratelimiter, SqlitePool), crate::Error> {
-    let rate_limiter = Ratelimiter::builder(60, Duration::from_secs(60))
-        .max_tokens(60)
-        .initial_available(60)
-        .build()?;
-    let dir = home_dir()
-        .map(|mut path| {
-            path.push(".wanisabi.db");
-            path.to_str().unwrap_or_default().to_owned()
-        })
-        .unwrap_or_default();
-    Ok((rate_limiter, SqlitePoolOptions::new().connect(&dir).await?))
+async fn rate_limiter_and_pool(
+    limit: bool,
+    pool: bool,
+) -> Result<(Option<Ratelimiter>, Option<SqlitePool>), crate::Error> {
+    let rate_limiter = if limit {
+        Some(
+            Ratelimiter::builder(60, Duration::from_secs(60))
+                .max_tokens(60)
+                .initial_available(60)
+                .build()?,
+        )
+    } else {
+        None
+    };
+    let db_pool = if pool {
+        let dir = home_dir()
+            .map(|mut path| {
+                path.push(".wanisabi.db");
+                path.to_str().unwrap_or_default().to_owned()
+            })
+            .unwrap_or_default();
+        Some(SqlitePoolOptions::new().connect(&dir).await?)
+    } else {
+        None
+    };
+    Ok((rate_limiter, db_pool))
 }
